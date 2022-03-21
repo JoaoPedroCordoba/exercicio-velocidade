f3a16f446a31d7a6770515d6a014a087f6593710
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 02:52:18 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double d, t, v;
+
+Console.WriteLine("digite a distancia(m), por favor:");
+d = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o tempo(s), por favor:");
+t = Convert.ToDouble(Console.ReadLine());
+
+v = d / t;
+
+Console.WriteLine($"A velocidade media é:{v:N2}m/s");
